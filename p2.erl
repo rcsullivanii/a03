825716a05a6c15466ecfedc0e545763d1b51ef21
Robert Sullivan <rsullivan@unc.edu>
2025-02-12 11:09:18 -0500

    Complete p2 module

--- p2.erl
+++ p2.erl
@@ -1,17 +1,19 @@
 % team: Robert Sullivan, Cole Vita
--module(p1).
--export([get_numData/0, tfac/1]).
+-module(p2).
+-export([loop_get_numData/0, tfac/1]).
 
-get_numData() ->
+loop_get_numData() ->
         io:format("Enter a number: "),
         case io:read("") of
             {ok, Num} when is_integer(Num) -> 
                 case Num of 
                     _ when Num < 0 -> 
                         Exp = round(math:pow(abs(Num), 7)),
-                        io:format("~w~n", [Exp]); 
+                        io:format("~w~n", [Exp]),
+                        loop_get_numData();
                     0 -> 
-                        io:format("~w~n", [Num]);
+                        io:format("Program exiting~n"),
+                        ok;
                     _ when Num > 0 -> 
                         if Num rem 7 == 0 -> 
                             FifthRoot = math:pow(Num, 1/5),
@@ -19,12 +21,15 @@ get_numData() ->
                         true -> 
                             Factorial = tfac(Num),
                             io:format("~w~n", [Factorial])
-                        end 
+                        end,
+                        loop_get_numData() 
                 end;
             {ok, _} -> 
-                io:format("Not an integer~n");
+                io:format("Not an integer~n"),
+                loop_get_numData();
             {error, _} -> % should this catch even inputs not ending in "."
-                io:format("Not a valid input~n")
+                io:format("Not a valid input~n"),
+                loop_get_numData()
     end.
 
 % In-class tail-recursive factorial function
